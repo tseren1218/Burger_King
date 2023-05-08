4bc410b6af4483e93e53d33c52f7a68c46a6794f
tseren1218 <tseren1218@gmail.com>
2023-05-08 18:48:34 +0800

    started cart

--- components/DeliveryProductCard.js
+++ components/DeliveryProductCard.js
@@ -20,11 +20,13 @@ class DeliveryProductCard extends HTMLElement {
         this.shadowRoot.innerHTML = `
             <link rel="stylesheet" href="./styles/delivery.css"/>
             <article id="delivery-item" class="delivery-item">
+                <div id="delivery-item-section">
                   <img class="delivery-image" height="200">
                   <h2 class="delivery-title"></h2>
                   <h4 class="delivery-calories"></h4>
                   <hr>
                   <p class="price"></p>
+                </div>
                   <a role="button" class="order-button" id="order-button">Сагсанд хийх</a>
             </article>
         `
@@ -35,7 +37,7 @@ class DeliveryProductCard extends HTMLElement {
     }
 
     connectedCallback() {
-        this.shadowRoot.getElementById("delivery-item").addEventListener("click", () => {
+        this.shadowRoot.getElementById("delivery-item-section").addEventListener("click", () => {
             this.insertAdjacentHTML("afterend", `
             <link id="style" rel="stylesheet" href="./styles/deliveryItemStyle.css">
             <div class="modal active" id="modal">
@@ -55,19 +57,18 @@ class DeliveryProductCard extends HTMLElement {
         }
         )
         this.shadowRoot.getElementById("order-button").addEventListener("click", () => {
-            this.shadowRoot.getElementById("delivery-item").removeEventListener()
             console.log("Added to cart");
         })
       
     }
 
     attributeChangedCallback(name, oldValue, newValue) {
-        this.shadowRoot.querySelector(".delivery-item .delivery-image").id = this.getAttribute("id");
-        this.shadowRoot.querySelector(".delivery-item .delivery-image").src = this.getAttribute("imgSrc");
-        this.shadowRoot.querySelector(".delivery-item .delivery-image").alt = this.getAttribute("imgAlt");
-        this.shadowRoot.querySelector(".delivery-item .delivery-title").innerHTML = this.getAttribute("productName");
-        this.shadowRoot.querySelector(".delivery-item .delivery-calories").innerHTML = this.getAttribute("productCalories");  
-        this.shadowRoot.querySelector(".delivery-item .price").innerHTML = this.getAttribute("productPrice");
+        this.shadowRoot.querySelector(".delivery-item #delivery-item-section .delivery-image").id = this.getAttribute("id");
+        this.shadowRoot.querySelector(".delivery-item #delivery-item-section .delivery-image").src = this.getAttribute("imgSrc");
+        this.shadowRoot.querySelector(".delivery-item #delivery-item-section .delivery-image").alt = this.getAttribute("imgAlt");
+        this.shadowRoot.querySelector(".delivery-item #delivery-item-section .delivery-title").innerHTML = this.getAttribute("productName");
+        this.shadowRoot.querySelector(".delivery-item #delivery-item-section .delivery-calories").innerHTML = this.getAttribute("productCalories");  
+        this.shadowRoot.querySelector(".delivery-item #delivery-item-section .price").innerHTML = this.getAttribute("productPrice");
     }   
 }
 
